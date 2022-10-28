d0adf6d14678eb5e5f5e8b4c0e5b98849c95cd6a
subreax <extode@gmail.com>
2022-10-28 16:20:20 +0300

    Theme configuration and auth layout improvements

--- src/App.svelte
+++ src/App.svelte
@@ -1,64 +1,65 @@
 <script>
     import Textfield from "@smui/textfield";
-    import Checkbox from '@smui/checkbox';
-    import FormField from '@smui/form-field';
-    import Button from '@smui/button';
-    import { Label } from '@smui/common';
+    import Checkbox from "@smui/checkbox";
+    import FormField from "@smui/form-field";
+    import Button from "@smui/button";
+    import { Label } from "@smui/common";
 
-    
-    let checked = false;
+    let remember = false;
     let email = "";
     let password = "";
+
+    function signInClicked() {
+        // вход кликнут
+    }
+
+    function signUpClicked() {
+        // регистрация кликнута
+    }
 </script>
 
-<body>
-    
-    <form class="container">
-        <figure class="icon">
-            <img src="../public/vdnh_icon.jpg" alt="">
-        </figure>
-        
-        <div class="auth-field">
-            <Textfield variant="outlined" bind:value={email} label="E-mail"/>
-        </div>
-    
-        <div class="auth-field">
-            <Textfield variant="outlined" bind:value={password} label="Пароль"/>
-        </div>
-        
-        <div class="forgot-password">
-            <Label><a href="">Забыли пароль?</a></Label>
-        </div>
-    
-        <div class="remember-me">
-            <FormField>
-                <Checkbox bind:checked />
-                <span slot="label">Запомнить меня</span>
-            </FormField>
-        </div>
-        
-    
-        <Button 
-            variant="raised" 
-            class="button-shaped-round"
-            style="background: #E22C38;">
-            <Label>Войти</Label>
-        </Button>
-    
-        <div>
-            <h2><span>или</span></h2>
-        </div>
+<form class="container">
+    <figure class="icon">
+        <img src="../public/vdnh_icon.jpg" alt="" />
+    </figure>
 
-        
-        <Button 
-            variant="raised" 
-            class="button-shaped-round"
-            style="background: #E22C38;">
-            <Label>Регистрация</Label>
-        </Button>
-    </form>
-</body>
+    <div class="auth-field">
+        <Textfield variant="outlined" bind:value={email} label="E-mail" />
+    </div>
 
+    <div class="auth-field">
+        <Textfield variant="outlined" bind:value={password} label="Пароль" />
+    </div>
+
+    <div class="forgot-password">
+        <Label><a href="#">Забыли пароль?</a></Label>
+    </div>
+
+    <div class="remember-me">
+        <FormField>
+            <Checkbox bind:checked={remember} />
+            <span slot="label">Запомнить меня</span>
+        </FormField>
+    </div>
+
+    <Button
+        variant="raised"
+        class="button-shaped-round"
+        on:click={(e) => {e.preventDefault(); signInClicked(); }}>
+        <Label>Войти</Label>
+    </Button>
+
+    <div>
+        <h2><span>или</span></h2>
+    </div>
+
+    <Button
+        variant="raised"
+        class="button-shaped-round"
+        on:click={(e) => { e.preventDefault(); signUpClicked(); }}>
+        <Label>Регистрация</Label>
+    </Button>
+</form>
 
 <style>
     * {
@@ -66,13 +67,19 @@
         padding: 0;
     }
 
-    .container {
-        box-sizing: border-box;
+    form {
         display: flex;
         flex-direction: column;
+        justify-content: center;
+
+        height: 100vh;
+        min-height: 700px;
+
+        min-width: 300px;
+        max-width: 500px;
+
         margin: 0 auto;
-        padding: 110px;
-        max-width: 762px;
+        padding: 0 32px;
     }
 
     .icon {
@@ -98,18 +105,17 @@
     }
 
     h2 {
-        margin: 20px 0 20px; 
-        text-align: center; 
-        border-bottom: 1px solid #000; 
+        margin: 20px 0 20px;
+        text-align: center;
+        border-bottom: 1px solid #000;
         line-height: 0.1em;
 
         font-weight: 400;
-    } 
-
-    h2 span { 
-        background:#fff; 
-        padding-top: 10px;
-        padding-bottom: 10px; 
     }
 
+    h2 span {
+        background: #fff;
+        padding: 10px;
+        font-size: 1.2rem;
+    }
 </style>
