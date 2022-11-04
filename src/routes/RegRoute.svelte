68fb77ec7f27bdbddbb6bc6327260740202e4be8
MsNefskiy <salionk223457162@gmail.com>
2022-11-04 14:48:50 +0300

    update registation view

--- src/routes/RegRoute.svelte
+++ src/routes/RegRoute.svelte
@@ -8,7 +8,6 @@
     let password;
     let passwordRepeated;
 
-
     function signInClicked() {
         // вход кликнут
     }
@@ -18,51 +17,71 @@
     }
 </script>
 
-<form>
-    <figure class="emblem">
-        <img src="../assets/Logo.svg" alt="" />
-    </figure>
+        <form class="container">
+            <picture class="emblem">
+                <img src="../assets/Logo.svg" alt="" />
+            </picture>
+        
+            <div class="fields">
+                <div class="textfields">
+                    <Textfield
+                        type="text"
+                        labelContent={"Email или телефон"}
+                        bind:inputContent={email}
+                    />
+                    <PasswordField
+                        idIcon={"icon1"}
+                        idPassword={"passwordWrite"}
+                        labelContent={"Пароль"}
+                        bind:inputContent={password}
+                    />
+                    <PasswordField
+                        idIcon={"icon2"}
+                        idPassword={"passwordRepite"}
+                        labelContent={"Повторить пароль"}
+                        bind:inputContent={passwordRepeated}
+                    />
+                </div>
+        
+                <div class="additional-buttons">
+                    <Checkbox checkboxContent="Запомнить меня" bind:status={remember} />
+                </div>
+            </div>
+        
+            <div class="buttons">
+                <button
+                    class="btn__raised btn-primary"
+                    on:click={(e) => {
+                        e.preventDefault();
+                        signInClicked();
+                    }}
+                >
+                    Зарегистрироваться
+                </button>
+        
+                <div class="btn-guest__container">
+                    <a href="" style="color: #E22C38;" class="additional-button"
+                        >Продолжить как гость</a
+                    >
+                </div>
+        
+                <div class="heading-with-lines">
+                    <span> или </span>
+                </div>
+        
+                <button
+                    class="btn-secondary"
+                    on:click={(e) => {
+                        e.preventDefault();
+                        signUpClicked();
+                    }}
+                >
+                    Войти
+                </button>
+            </div>
+        </form>
 
-    <div class="fields">
-        <div class="textfields">
-            <Textfield type="text" labelContent={"Email или телефон"} bind:inputContent={email}/>
-            <PasswordField idIcon={"icon1"} idPassword={"passwordWrite"} labelContent={"Пароль"} bind:inputContent={password}/>
-            <PasswordField idIcon={"icon2"} idPassword={"passwordRepite"} labelContent={"Повторить пароль"} bind:inputContent={passwordRepeated}/>
-        </div>
 
-        <div class="additional-buttons">
-            <Checkbox checkboxContent="Запомнить меня" bind:status={remember}/>
-        </div> 
-    </div>
-
-
-    <div class="buttons">
-        <button class="btn__raised btn-primary" 
-        on:click={(e) => {
-            e.preventDefault();
-            signInClicked();
-        }}>
-            Зарегистрироваться
-        </button>
-
-        <div class="btn-guest__container">
-            <a href="" style="color: #E22C38;" class="additional-button">Продолжить как гость</a>
-        </div>
-
-        <div class="heading-with-lines">
-            <span> или </span>
-        </div>
-
-        <button class="btn-secondary" 
-        on:click={(e) => {
-            e.preventDefault();
-            signUpClicked();
-        }}>
-            Войти
-        </button>
-
-    </div>
-</form>
 
 <style>
     .fields {
